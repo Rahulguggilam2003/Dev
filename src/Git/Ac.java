42c62730defa75de79dba33d66bd9000dd7553f0
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:56:32 +0530

    First Commit

---
+++ src/Git/Ac.java
@@ -0,0 +1,15 @@
+package Git;
+import java.util.*;
+public class Ac {
+	//area of circle,rectangle,triangle function names,ac,rec,tri
+	public double Ac(int rad) {
+		return 3.14*rad*rad;
+	}
+
+	public int Rec(int x,int y) {
+		return x*y;
+	}
+	public double tri(int x,int y) {
+		return 0.5*x*y;
+	}
+}
---
+++ src/Git/Ar.java
@@ -0,0 +1,17 @@
+package Git;
+import java.util.*;
+//add,mul,sub,div
+public class Ar {
+	public int add(int x,int y) {
+		return x+y;
+	}
+	public int mul(int x,int y) {
+		return x*y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
+	}
+	public int div(int x,int y) {
+		return x/y;
+	}
+}
